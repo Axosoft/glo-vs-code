fe2d897504e3571685c0f070bf9d1a0f8c619d3e
Unknown <joseg@axosoft.com>
2017-09-13 13:13:34 -0700

    add Jenkins files

---
+++ build/shared/build.groovy
@@ -0,0 +1,44 @@
+def build_app_bundle(configuration) {
+  // pull shared UI
+  sshagent (credentials: ['axosoft-build']) {
+    // install depencencies
+    sh "git clone git@github.com:Axosoft/app.gitkraken.com.git"
+  
+    dir ('app.gitkraken.com') {
+      sh 'yarn'
+
+      // HACK: build shared styles
+      dir("./node_modules/gitkraken-shared-styles") {
+        sh 'yarn build'
+      }
+      // HACK: copy gk-shared-styles to tasks-ui node_modules
+      sh "cp -R ./node_modules/gitkraken-shared-styles ./node_modules/getcrakin-shared/node_modules"
+
+      // copy the config.json for tasks-ui package before building it
+      sh "cp ./node_modules/getcrakin-shared/build/${configuration}/config.json ./node_modules/getcrakin-shared/config.json"
+
+      // build
+      sh 'yarn build'
+    }
+  }
+}
+
+def build_vs_code(configuration) {
+  // copy bundle to extension src folder
+  sh 'cp app.gitkraken.com/static/bundle.js src/glo.js'
+
+  // build the extension file
+  sh 'vsce package'
+
+  def extensionFilename = sh (
+    script: 'find *.vsix',
+    returnStdout: true
+  ).trim()
+
+  // archive artifact
+  archiveArtifacts artifacts: extensionFilename
+
+  return extensionFilename
+}
+
+return this
